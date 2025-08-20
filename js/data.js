4ff93cfbc7aa3d9378f7324a47cd45df1c2eadab
fhpeerless <126315532+fhpeerless@users.noreply.github.com>
2025-08-21 06:21:52 +0800

    Update data.js

--- js/data.js
+++ js/data.js
@@ -2,7 +2,7 @@
 export const lettersData = [
     {
         id: 1,
-        title: "让我快一些遇见你！",
+        title: "让我快一些遇见你！sum=0(代表此帖此时星星的数量)",
         date: "2025-08-18",
         text: "我想快一些遇见你，这样世界就少了两个孤单的人！\n 在历史的长河中，十年如沧海一粟，但在人生的旅途中，屈指可数，\n 花开堪折直须折 莫待无花空折枝 \n 今日星星数量为0",
         photos: [
@@ -12,9 +12,9 @@ export const lettersData = [
     },
     {
         id: 2,
-        title: "我的照片",
+        title: "我的照片sum=0",
         date: "2025-08-20",
-        text: "去干饭吧！去运动吧，去看剧吧，去逛逛大街吧，嗯，只不过是一个人！",
+        text: "去干饭吧！去运动吧，去看剧吧，去逛逛大街吧，嗯，只不过是一个人！，",
         photos: [
             "./images/5b50a9502e745b6480ee27afafe17bf.jpg",
             "./images/5cda81b90c55443835d42f0fb073b53.jpg",
