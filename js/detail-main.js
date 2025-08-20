7807d9d5a1be5bf9810d18c7667417a312779d7c
fhpeerless <126315532+fhpeerless@users.noreply.github.com>
2025-08-20 14:12:42 +0800

    Update detail-main.js

--- js/detail-main.js
+++ js/detail-main.js
@@ -1,9 +1,9 @@
 // js/detail-main.js
-import { startTimer } from '.js/timer.js';
-import { displayArticle } from '.js/article.js';
-import { toggleProfile, toggleContact } from '.js/sidebar.js';
-import { startHeartAnimation } from '.js/heart.js';
-import { lettersData } from '.js/data.js';
+import { startTimer } from './js/timer.js';
+import { displayArticle } from './js/article.js';
+import { toggleProfile, toggleContact } from './js/sidebar.js';
+import { startHeartAnimation } from './js/heart.js';
+import { lettersData } from './js/data.js';
 
 // 暴露数据到全局
 window.lettersData = lettersData;
