246615d29fe89b5957aea6a3690f8b0c516471e9
fhpeerless <126315532+fhpeerless@users.noreply.github.com>
2025-08-20 12:29:10 +0800

    Update letter.js

--- js/letter.js
+++ js/letter.js
@@ -1,4 +1,4 @@
-// letter.js
+// js/letter.js
 import { lettersData } from './data.js';
 import { formatDate } from './utils.js';
 
@@ -6,11 +6,13 @@ export function generateLetters() {
     const letterList = document.getElementById('letterList');
     letterList.innerHTML = '';
 
-    lettersData.forEach(letter => {
+    lettersData.forEach((letter, index) => {  // ✅ 解构出 index
         const li = document.createElement('li');
         li.className = 'letter-item';
+
+        // ✅ 点击时存储的是索引 index，而不是 letter.id
         li.onclick = () => {
-            localStorage.setItem('currentLetterId', letter.id);
+            localStorage.setItem('currentLetterId', index);
             window.location.href = 'article.html';
         };
 
