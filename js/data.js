1c6dfd12920d027caad529e0357ca4f2b40def41
fhpeerless <126315532+fhpeerless@users.noreply.github.com>
2025-08-20 18:34:01 +0800

    Update data.js

--- js/data.js
+++ js/data.js
@@ -4,7 +4,7 @@ export const lettersData = [
         id: 1,
         title: "让我快一些遇见你！",
         date: "2025-01-01",
-        text: "我想快一些遇见你，这样世界就少了两个孤单的人！\n 在历史的长河中，十年如沧海一粟，但在人生的旅途中，屈指可数，\n 花开堪折直须折 莫待无花空折枝",
+        text: "我想快一些遇见你，这样世界就少了两个孤单的人！\n 在历史的长河中，十年如沧海一粟，但在人生的旅途中，屈指可数，\n 花开堪折直须折 莫待无花空折枝 \n 今日星星数量为0",
         photos: [
             "https://picsum.photos/seed/love1/400/300",
             "https://picsum.photos/seed/love2/400/300"
@@ -12,9 +12,9 @@ export const lettersData = [
     },
     {
         id: 2,
-        title: "让我快一些遇见你",
+        title: "星星很亮",
         date: "2025-01-01",
-        text: "让我快一些遇见你， 头发特瑞特",
+        text: "让我快一些遇见你",
         photos: [
             "https://picsum.photos/seed/love1/400/300",
             "https://picsum.photos/seed/love2/400/300"
