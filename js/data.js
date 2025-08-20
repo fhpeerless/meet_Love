683e8bc895a702a8af83f7094df7c480ed499e78
fhpeerless <126315532+fhpeerless@users.noreply.github.com>
2025-08-20 17:25:31 +0800

    Update data.js

--- js/data.js
+++ js/data.js
@@ -2,9 +2,9 @@
 const lettersData = [
     {
         id: 1,
-        title: "我的伴侣你何时能出现",
+        title: "你早点出现吧，我一个人承受不来！",
         date: "2025-01-01",
-        text: "我想因为有爱，两个人才能长相守吧，那到底什么是爱呢！\n\n是虞姬为爱忠贞赴死吗，是梁山伯病逝后，祝英台化蝶吗\n 这凄美的爱情我不赞扬，我想只要稳稳的幸福就够了，我只是一界外卖员，",
+        text: "我想因为有爱，两个人才能长相守吧，那到底什么是爱呢！\n\n是虞姬为爱忠贞赴死吗，是梁山伯病逝后，祝英台化蝶吗\n 这凄美的爱情我不赞扬，我也只是个凡人，",
         photos: [
             "https://picsum.photos/seed/love1/400/300",
             "https://picsum.photos/seed/love2/400/300"
@@ -12,9 +12,9 @@ const lettersData = [
     },
         {
         id: 2,
-        title: "心情阴晴不定！",
+        title: "！",
         date: "2025-01-01",
-        text: "亲爱的，\n\n遇见你的那一刻，我的世界开始发光...永远爱你的\n我",
+        text: "让我快一些遇见你，\n\n\n",
         photos: [
             "https://picsum.photos/seed/love1/400/300",
             "https://picsum.photos/seed/love2/400/300"
