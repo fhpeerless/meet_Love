3217d91c44693b3ee96b92ec2315f68b4bd52b5d
fhpeerless <126315532+fhpeerless@users.noreply.github.com>
2025-08-20 18:02:36 +0800

    Update data.js

--- js/data.js
+++ js/data.js
@@ -1,28 +1,27 @@
-// data.js
-const lettersData = [
+// js/data.js
+export const lettersData = [
     {
         id: 1,
         title: "你早点出现吧，我一个人承受不来！",
         date: "2025-01-01",
-        text: "我想因为有爱，两个人才能长相守吧，那到底什么是爱呢！\n\n是虞姬为爱忠贞赴死吗，是梁山伯病逝后，祝英台化蝶吗\n 这凄美的爱情我不赞扬，我也只是个凡人，",
+        text: "我想因为有爱，两个人才能长相守吧，那到底什么是爱呢！\n是虞姬为爱忠贞赴死吗，是梁山伯病逝后，祝英台化蝶吗\n 这凄美的爱情我不赞扬，我也只是个凡人，",
         photos: [
             "https://picsum.photos/seed/love1/400/300",
             "https://picsum.photos/seed/love2/400/300"
         ]
     },
-  {
+    {
         id: 2,
-        title: "！",
+        title: "让我快一些遇见你",
         date: "2025-01-01",
         text: "让我快一些遇见你， 头发特瑞特",
         photos: [
             "https://picsum.photos/seed/love1/400/300",
             "https://picsum.photos/seed/love2/400/300"
         ]
-  }  
-    // ...其他信件数据
+    }
+    // ... 其他信件
 ];
-window.lettersData = lettersData;
 
-// data.js
-console.log('✅ data.js 已成功加载');
+// ✅ 移除了：window.lettersData = lettersData;
+// ✅ 移除了：console.log('✅ data.js 已成功加载');
