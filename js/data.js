c54481eb1ef6cceb6ada95ad46aac88b3baedc70
fhpeerless <126315532+fhpeerless@users.noreply.github.com>
2025-08-20 08:49:17 +0800

    Update and rename N0ts.js to data.js

---
+++ js/data.js
@@ -0,0 +1,14 @@
+// data.js
+export const lettersData = [
+    {
+        id: 1,
+        title: "第一封情书",
+        date: "2025-01-01",
+        text: "亲爱的，\n\n遇见你的那一刻，我的世界开始发光...永远爱你的\n我",
+        photos: [
+            "https://picsum.photos/seed/love1/400/300",
+            "https://picsum.photos/seed/love2/400/300"
+        ]
+    },
+    // ...其他信件数据
+];
