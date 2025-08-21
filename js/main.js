7338c0d0129c9d162ebd3f74511735d5994cc557
fhpeerless <126315532+fhpeerless@users.noreply.github.com>
2025-08-21 18:41:51 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -34,7 +34,7 @@ function initBackgroundMusic() {
     bgMusicPlayer.setMusic(
        '孙燕姿-遇见', 
         'http://note.youdao.com/yws/api/personal/file/1f3ec446fd52ecd683be5c509aebf58d?method=download&inline=true&shareKey=fc9eac5d25590b1c61a9d8a9450d653a',
-        './lrc/孙燕姿 - 遇见.lrc' // 您的LRC歌词文件URL
+        './lrc/yujian.lrc' // 您的LRC歌词文件URL
     );
 }
 
