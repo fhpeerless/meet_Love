07d5aae1dbdeacd95aac705464c32a1ff550922d
fhpeerless <126315532+fhpeerless@users.noreply.github.com>
2025-08-23 07:52:01 +0800

    Update detail-main.js

--- js/detail-main.js
+++ js/detail-main.js
@@ -4,6 +4,8 @@ import { displayArticle } from './article.js'; // ✅ 导入 displayArticle
 import { toggleProfile, toggleContact } from './sidebar.js';
 import { startHeartAnimation } from './heart.js';
 
+let bgMusicPlayer = window.bgMusicPlayer || new BackgroundMusicPlayer();
+
 // 将侧边栏和显示文章函数暴露到全局
 window.toggleProfile = toggleProfile;
 window.toggleContact = toggleContact;
@@ -14,4 +16,14 @@ document.addEventListener('DOMContentLoaded', () => {
     displayArticle(); // ✅ 调用 displayArticle
     startTimer();
     startHeartAnimation(150);
+
+    if (!bgMusicPlayer) {
+        bgMusicPlayer = new BackgroundMusicPlayer();
+        bgMusicPlayer.setMusic(
+            '孙燕姿-遇见', 
+            'http://note.youdao.com/yws/api/personal/file/1f3ec446fd52ecd683be5c509aebf58d?method=download&inline=true&shareKey=fc9eac5d25590b1c61a9d8a9450d653a',
+            './lrc/yujian.lrc'
+        );
+        bgMusicPlayer.play();
+    }
 });
